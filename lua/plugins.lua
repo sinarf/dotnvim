15c07fe9a106637e4042926e95890feb55f18b01
sinarf <sinarf@sinarf.org>
2021-07-11 10:22:49 +0200

    Added telescope-github extension

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,6 +21,7 @@ return require('packer').startup(function(use)
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
     }
     use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use 'nvim-telescope/telescope-github.nvim'
 
     -- File explorer
     use {
