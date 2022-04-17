5dd13d30bc3e254ce867e6cce45134f163adcd2b
sinarf <sinarf@sinarf.org>
2022-04-17 13:30:37 +0200

    Adding completion with cmp

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require "conf.options"
 require "conf.keymaps"
 require "conf.plugins"
+require "conf.cmp"
 
 -- TODO add the secret file
--- lua/conf/plugins.lua
+++ lua/conf/plugins.lua
@@ -47,7 +47,16 @@ return packer.startup(function(use)
 
   -- Development
   use "airblade/vim-rooter"
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
 
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
   -- eye candy
   use {'dracula/vim', as = 'dracula'}
   -- Automatically set up your configuration after cloning packer.nvim
