2dbd449df6389ce3b83ec80ed88cb37b26e821a5
sinarf <sinarf@sinarf.org>
2022-04-25 10:25:49 +0200

    Cleanier options

--- lua/conf/options.lua
+++ lua/conf/options.lua
@@ -1,20 +1,25 @@
+-- global neovim options 
 vim.o.clipboard = "unnamedplus"
 vim.o.backup = false
 vim.o.fileencoding = "utf-8"
-vim.o.ignorecase = true
 
+-- search
 vim.o.hlsearch = true
 vim.o.smartcase = true
-
-vim.o.mouse = "a"
+vim.o.ignorecase = true
 
 vim.o.number = true
 vim.o.signcolumn = "yes"
 
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
-vim.o.guifont = "Cascadia Code:h14"
 
+termguicolors = true
+vim.o.guifont = "Cascadia Code:h14"
+vim.o.mouse = "a"
+vim.o.colorcolumn = 120
+
+-- spelling disable by default
 vim.o.spelllang = "en,fr"
 vim.o.spell = false
 
@@ -22,4 +27,3 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
-vim.o.colorcolumn = 120
