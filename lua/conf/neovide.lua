a3c929c241408855565eea0c2cb3ae7e06fbd119
sinarf <sinarf@sinarf.org>
2022-06-09 14:29:14 +0200

    doc link

--- lua/conf/neovide.lua
+++ lua/conf/neovide.lua
@@ -1,5 +1,7 @@
 -- neovide configuration
+-- Details https://github.com/neovide/neovide/wiki/Configuration
 vim.cmd [[
     let g:neovide_cursor_antialiasing=v:true
     let g:neovide_transparency=0.9
+    let g:neovide_fullscreen=v:false
 ]]
