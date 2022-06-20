db43c87c2ff661be1999a9d1871ef0d69f2a4052
sinarf <sinarf@sinarf.org>
2022-06-20 11:06:08 +0000

    Add new keymaps for fugitives

--- lua/conf/fugitive.lua
+++ lua/conf/fugitive.lua
@@ -1,20 +1,11 @@
 -- fugitive plugin configuration
 
 local opts = { noremap = true, silent = true }
-local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<leader>gg", ":Git<CR>", opts)
-
-
-local fugitive_group = vim.api.nvim_create_augroup("shell", { clear = true })
-
--- perform a git fetch when using fugitive
-vim.api.nvim_create_autocmd(
-    "FileType", {
-        group = fugitive_group,
-        pattern = "fugitive",
-        command = ":Git fetch",
-})
+keymap("n", "<leader>gf", ":Git fetch<CR>", opts)
+keymap("n", "<leader>gst", ":Git stash<CR>", opts)
+keymap("n", "<leader>gsp", ":Git stash pop<CR>", opts)
