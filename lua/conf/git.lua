2479f4147adedef6f36cdf9a39e18231410f6335
sinarf <sinarf@sinarf.org>
2022-07-13 12:09:33 +0200

    Set spell on git commit

--- lua/conf/git.lua
+++ lua/conf/git.lua
@@ -12,3 +12,10 @@ keymap("n", "<leader>gsp", ":Git stash pop<CR>", opts)
 
 -- git signs
 require('gitsigns').setup()
+
+local git_group = vim.api.nvim_create_augroup("git", { clear = true })
+vim.api.nvim_create_autocmd( "FileType", {
+    group = git_group,
+    pattern = "gitcommit",
+    command = "setlocal spell",
+})
