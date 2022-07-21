8f0d775837334e7a8640d4cc377f08fabc4c574f
sinarf <sinarf@sinarf.org>
2022-07-21 09:12:58 +0200

    Save before running fugitive

--- lua/conf/git.lua
+++ lua/conf/git.lua
@@ -5,7 +5,7 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<leader>gg", ":Git<CR>", opts)
+keymap("n", "<leader>gg", ":update | Git<CR>", opts)
 keymap("n", "<leader>gf", ":Git fetch<CR>", opts)
 keymap("n", "<leader>gst", ":Git stash<CR>", opts)
 keymap("n", "<leader>gsp", ":Git stash pop<CR>", opts)
@@ -15,7 +15,7 @@ keymap("n", "<leader>gl", ":Git log<CR>", opts)
 require('gitsigns').setup()
 
 local git_group = vim.api.nvim_create_augroup("git", { clear = true })
-vim.api.nvim_create_autocmd( "FileType", {
+vim.api.nvim_create_autocmd("FileType", {
     group = git_group,
     pattern = "gitcommit",
     command = "setlocal spell",
