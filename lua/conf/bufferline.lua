c04c798d55431a648f3945eee9ef483d5e847250
sinarf <sinarf@sinarf.org>
2022-07-21 11:39:42 +0200

    Exclude fugitive window

--- lua/conf/bufferline.lua
+++ lua/conf/bufferline.lua
@@ -2,6 +2,14 @@ require('bufferline').setup {
     options = {
         -- numbers = "ordinal",
         diagnostics = "nvim_lsp",
-        separator_style = "slant"
+        separator_style = "slant",
+        custom_filter = function(buf_number)
+            if vim.bo[buf_number].filetype ~= "fugitive" then
+                return true
+            end
+            -- if vim.bo[buf_number].filetype ~= "gitcommit" then
+            --     return true
+            -- end
+        end,
     }
 }
