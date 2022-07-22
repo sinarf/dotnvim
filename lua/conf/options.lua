40f5234098962327fbc4748ee746b1556bfa8f12
sinarf <sinarf@sinarf.org>
2022-07-22 17:34:44 +0200

    You can see me

--- lua/conf/options.lua
+++ lua/conf/options.lua
@@ -28,3 +28,13 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.splitbelow = true
+
+vim.o.list        = true
+vim.opt.listchars = {
+    tab      = "▶▷",
+    eol      = "↲",
+    trail    = "✖",
+    extends  = "❯",
+    precedes = "❮",
+    space    = "·",
+}
