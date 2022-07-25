3a5d3117938bb60cbb814beeb7f8cc235ea67b9e
sinarf <sinarf@sinarf.org>
2022-07-25 10:53:50 +0200

    Code format

--- lua/conf/autoformat.lua
+++ lua/conf/autoformat.lua
@@ -1,7 +1,10 @@
 -- enable autoformatting on some filetypes
 --
 local format_group       = vim.api.nvim_create_augroup("format", { clear = true })
-local filetypes_patterns = { "*.lua", "*.py" }
+local filetypes_patterns = {
+    "*.lua",
+    "*.py",
+}
 
 for _, filetype_pattern in ipairs(filetypes_patterns) do
     vim.api.nvim_create_autocmd("BufWritePre", {
