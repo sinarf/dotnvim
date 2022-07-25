6c12a2122d38e4621325b6ad45d0232b5e545e9d
sinarf <sinarf@sinarf.org>
2022-07-25 14:09:47 +0200

    Handle zoom in Neovide

--- lua/conf/neovide.lua
+++ lua/conf/neovide.lua
@@ -5,3 +5,33 @@ vim.cmd [[
     let g:neovide_transparency=0.9
     let g:neovide_fullscreen=v:false
 ]]
+
+-- Handle the zoom, sourced from: https://github.com/neovide/neovide/issues/1301#issuecomment-1119370546
+vim.g.gui_font_default_size = 14
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "Fira Code"
+
+RefreshGuiFont = function()
+    vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+    vim.g.gui_font_size = vim.g.gui_font_size + delta
+    RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+    vim.g.gui_font_size = vim.g.gui_font_default_size
+    RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Keymaps
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({ 'n', 'i' }, "<C-=>", function() ResizeGuiFont(1) end, opts)
+vim.keymap.set({ 'n', 'i' }, "<C-->", function() ResizeGuiFont(-1) end, opts)
+vim.keymap.set({ 'n', 'i' }, "<C-BS>", function() ResetGuiFont() end, opts)
