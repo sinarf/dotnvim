a3c9f3a4c84b725f6f5115dec8d2abb9b37af99a
sinarf <sinarf@sinarf.org>
2022-07-29 12:13:52 +0200

    Format code

--- lua/conf/autosave.lua
+++ lua/conf/autosave.lua
@@ -1,6 +1,5 @@
 local status_ok, autosave = pcall(require, "autosave")
 if not status_ok then
-
     return
 end
 
@@ -8,7 +7,10 @@ autosave.setup(
     {
         enabled = true,
         execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
-        events = { "InsertLeave", "TextChanged" },
+        events = {
+            "InsertLeave",
+            "TextChanged"
+        },
         conditions = {
             exists = true,
             filename_is_not = { 'plugins.lua' },
