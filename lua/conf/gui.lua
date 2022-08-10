e0f44c149c5f510a8b6817ceb18038f99c702a7a
sinarf <sinarf@sinarf.org>
2022-08-10 14:05:43 +0200

    Going back to FiraCode for ligatures

--- lua/conf/gui.lua
+++ lua/conf/gui.lua
@@ -1,21 +1,23 @@
 -- neovide configuration
 -- Details https://github.com/neovide/neovide/wiki/Configuration
 vim.cmd [[
-    let g:neovide_cursor_antialiasing=v:true
-    let g:neovide_transparency=0.9
-    let g:neovide_fullscreen=v:false
-    let g:neovide_cursor_vfx_mode = "railgun"
-    let g:neovide_cursor_vfx_particle_lifetime=2
-    let g:neovide_cursor_vfx_particle_density=7.0
-    let g:neovide_cursor_vfx_particle_speed=10.0
-    let g:neovide_cursor_vfx_particle_curl=1.0
+    if exists("g:neovide")
+        let g:neovide_cursor_antialiasing=v:true
+        let g:neovide_transparency=0.9
+        let g:neovide_fullscreen=v:false
+        let g:neovide_cursor_vfx_mode = "railgun"
+        let g:neovide_cursor_vfx_particle_lifetime=2
+        let g:neovide_cursor_vfx_particle_density=7.0
+        let g:neovide_cursor_vfx_particle_speed=10.0
+        let g:neovide_cursor_vfx_particle_curl=1.0
+    endif
 ]]
 
 -- Handle the zoom, sourced from: https://github.com/neovide/neovide/issues/1301#issuecomment-1119370546
 vim.g.gui_font_default_size = 16
 vim.g.gui_font_size = vim.g.gui_font_default_size
 -- vim.g.gui_font_face = "FiraCode Nerd Font"
-vim.g.gui_font_face = "Hack Nerd Font"
+vim.g.gui_font_face = "FiraCode Nerd Font"
 RefreshGuiFont = function()
     vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
 end
