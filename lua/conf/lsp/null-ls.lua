2534d9cfc1aad249ecfd5b9194aa5993b71110e0
sinarf <sinarf@sinarf.org>
2022-10-05 09:41:30 +0200

    Add new formatting tools
    Added format for shell script and groovy.

--- lua/conf/lsp/null-ls.lua
+++ lua/conf/lsp/null-ls.lua
@@ -14,10 +14,10 @@ null_ls.setup {
         formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
         -- preview to split lines that are to long (will be default some day)
         formatting.black.with { extra_args = { "--experimental-string-processing" } },
-
+        formatting.shfmt,
+        formatting.npm_groovy_lint,
         diagnostics.gitlint,
         -- disable warning line length warning
         diagnostics.markdownlint.with { extra_args = { "--disable MD013" } },
-        null_ls.builtins.code_actions.gitsigns,
     },
 }
