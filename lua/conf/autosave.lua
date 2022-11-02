ddb9d3f3c4c6ecc1fa5708546bf3daeb4dfed536
sinarf <sinarf@sinarf.org>
2022-11-02 14:09:11 +0100

    Exclusion from autosave
    * gitcommit filetype because this buffer is transient by definition
    * plugins.lua because saving triggers the installation

--- lua/conf/autosave.lua
+++ lua/conf/autosave.lua
@@ -23,8 +23,11 @@ autosave.setup(
         condition = function(buf)
             local fn = vim.fn
             local utils = require("auto-save.utils.data")
+            if fn.bufname(buf) == "lua/conf/plugins.lua" then
+                return false
+            end
             if fn.getbufvar(buf, "&modifiable") == 1 and
-                utils.not_in(fn.getbufvar(buf, "&filetype"), {'lua'}) then
+                utils.not_in(fn.getbufvar(buf, "&filetype"), { 'gitcommit' }) then
                 return true -- met condition(s), can save
             end
             return false -- can't save
