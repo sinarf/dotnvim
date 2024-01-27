ce43862ecc47b5654ff4d6c48c910ed481d0d6e2
sinarf <sinarf@sinarf.org>
2024-01-27 08:05:22 +0100

    Update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -96,7 +96,6 @@ require('lazy').setup({
             "nvim-lua/plenary.nvim",         -- required
             "nvim-telescope/telescope.nvim", -- optional
             "sindrets/diffview.nvim",        -- optional
-            "ibhagwan/fzf-lua",              -- optional
         },
         config = true
     },
