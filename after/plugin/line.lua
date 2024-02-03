6c314b315359a68737e8d6577b98a3cccf3deee0
sinarf <sinarf@sinarf.org>
2024-02-03 20:24:54 +0100

    ricing lualine

--- after/plugin/line.lua
+++ after/plugin/line.lua
@@ -1,5 +1,25 @@
-require('lualine').setup {
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+    vim.notify("Could not load module lualine!")
+    return
+end
+
+lualine.setup {
     options = {
-        theme = "catppuccin"
-    }
+        theme = "catppuccin",
+        component_separators = {
+            left = ' ',
+            right = ' ',
+        },
+        section_separators = {
+            left = ' ',
+            right = ' ',
+        },
+    },
+    extensions = {
+        'mason',
+        'oil',
+        'quickfix',
+        'trouble',
+    },
 }
