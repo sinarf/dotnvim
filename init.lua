bef81307395d197cb767a01e79e01fe7419ccafc
sinarf <sinarf@sinarf.org>
2024-02-04 13:34:09 +0100

    Remove autosourcing of config

--- init.lua
+++ init.lua
@@ -2,10 +2,3 @@ require("plugins")
 require("colorscheme")
 require("options")
 require("keymaps")
-
-local init_group = vim.api.nvim_create_augroup("init_group", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = init_group,
-  pattern = "*/.config/nvim/**/*.lua",
-  command = "source $MYVIMRC",
-})
